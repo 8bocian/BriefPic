e20b102c893fdb3517d31672d57d6c87fe8aaa05
Stork <74800537+8bocian@users.noreply.github.com>
2023-03-02 23:02:17 +0100

    fix

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json
-import cv2.cv2 as cv2
 import numpy as np
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
@@ -35,41 +34,11 @@ class Summarizer(Resource):
         heightRatio = request.form['heighthRatio']
         image_stream = request.files['image'].stream
 
-        # if request.files['type'] == "bulletpoints":
-        #     prefix = "Stwórz punkty na podstawie tekstu: "
-        # else:
-        #     prefix = "Podsumuj tekst: "
         prefix = "Podsumuj tekst: "
-        # image_bytes.save("image.jpg")
-        # image = cv2.imread("image.jpg")
-        # image_stream = io.BytesIO(image_bytes)
+
         image_stream.seek(0)
         file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
         image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
         text = pip.fullRun(image, widthRatio, heightRatio, prefix, mask)
         print(text)
         return createResponse(text, 200)
-
-
-# app = FastAPI()
-#
-#
-# class ImageType(BaseModel):
-#     url: str
-#
-#
-# @app.post("/predict/")
-# def prediction(request: Request, file: bytes = File):
-#     if request.method == "POST":
-#         image_stream = io.BytesIO(file)
-#         image_stream.seek(0)
-#         file_bytes = np.asarray(bytearray(image_stream.read()), dtype=np.uint8)
-#         image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-#         widthRatio = 0
-#         heightRatio = 0
-#         prefix = ""
-#         mask = None
-#         text = pip.fullRun(image, widthRatio, heightRatio, prefix, mask)
-#
-#         return text
-#     return "Method Not Allowed"
